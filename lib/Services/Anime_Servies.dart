71c740b67a42250de9209e940b0999db0eda9f58
Manish-git-135 <mmanishmoond2000@gmail.com>
2021-07-17 14:46:07 +0530

    api cache manager added to top and this season in anime😎

--- lib/Services/Anime_Servies.dart
+++ lib/Services/Anime_Servies.dart
@@ -1,4 +1,6 @@
 import 'dart:convert';
+import 'package:api_cache_manager/api_cache_manager.dart';
+import 'package:api_cache_manager/models/cache_db_model.dart';
 import 'package:http/http.dart' as http;
 import 'package:sam_frontend/Models/Anime_Model.dart';
 import 'package:sam_frontend/Models/Anime_RR_Model.dart';
@@ -8,14 +10,25 @@ import 'package:sam_frontend/Models/Anime_Top_Model.dart';
 
 class HttpAnimeServices {
   Future<AnimeTopModel> getTopAnime() async {
-    final res = await http
-        .get(Uri.https('sam-api-flask.herokuapp.com', '/anime/toprated'));
-    if (res.statusCode == 200) {
-      AnimeTopModel resser = AnimeTopModel.fromJson(json.decode(res.body));
-      return resser;
+    var isCacheExist =
+        await APICacheManager().isAPICacheKeyExist('AnimeTopAnime');
+    if (!isCacheExist) {
+      final res = await http
+          .get(Uri.https('sam-api-flask.herokuapp.com', '/anime/toprated'));
+      if (res.statusCode == 200) {
+        // Adding data to local DB for performance
+        APICacheDBModel cacheDBModel =
+            new APICacheDBModel(key: 'AnimeTopAnime', syncData: res.body);
+        APICacheManager().addCacheData(cacheDBModel);
+
+        AnimeTopModel resser = AnimeTopModel.fromJson(json.decode(res.body));
+        return resser;
+      }
     } else {
-      throw Exception("Failed to fetch Anime");
+      var cacheData = await APICacheManager().getCacheData('AnimeTopAnime');
+      return AnimeTopModel.fromJson(json.decode(cacheData.syncData));
     }
+    throw Exception("Failed to fetch Anime");
   }
 
   Future<AnimeModel> getAnime({required int malId}) async {
@@ -39,13 +52,26 @@ class HttpAnimeServices {
 
   Future<AnimeThisSeasonModel> getThisSeason(
       {required int year, required String season}) async {
-    final res = await http.get(Uri.https(
-        'sam-api-flask.herokuapp.com', '/anime/byseason/$year/$season'));
-    if (res.statusCode == 200) {
-      AnimeThisSeasonModel result =
-          AnimeThisSeasonModel.fromJson(json.decode(res.body));
-      return result;
+    var isCacheExist =
+        await APICacheManager().isAPICacheKeyExist('AnimeThisSeason');
+    if (!isCacheExist) {
+      final res = await http.get(Uri.https(
+          'sam-api-flask.herokuapp.com', '/anime/byseason/$year/$season'));
+      if (res.statusCode == 200) {
+        // Adding data to local DB for performance
+        APICacheDBModel cacheDBModel =
+            new APICacheDBModel(key: 'AnimeThisSeason', syncData: res.body);
+        APICacheManager().addCacheData(cacheDBModel);
+
+        AnimeThisSeasonModel result =
+            AnimeThisSeasonModel.fromJson(json.decode(res.body));
+        return result;
+      }
+    } else {
+      var cacheData = await APICacheManager().getCacheData('AnimeThisSeason');
+      return AnimeThisSeasonModel.fromJson(json.decode(cacheData.syncData));
     }
+
     throw Exception('Error');
   }
 
